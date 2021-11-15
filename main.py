a8a1c0f547689e568742772066e073076240175b
Romimap <romain.fournier.095@gmail.com>
2021-11-15 09:53:19 +0100

    test code

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import sys
 import os
 import open3d
+import bitarray
+
+import Code
+from Code.Quantization import quantizeVertices
 
 
 def testFunction():
 	print("Testing IO for meshes ...")
 	mesh = open3d.io.read_triangle_mesh("Models/bunny.obj")
-	print(mesh)
+	mesh.vertices, bitstring = quantizeVertices(mesh.vertices, 3)
+
+	mesh.compute_vertex_normals()
+	open3d.visualization.draw_geometries([mesh])
+
+	bitArray = bitarray.bitarray([0 for _ in range(8-(len(bitstring)%8))] + list(map(int,bitstring)))
+
+	with open("bits","wb+") as f:
+		bitArray.tofile(f)
 
 
 def main():
@@ -15,7 +27,6 @@ def main():
 	testFunction()
 
 	return 0
-
 
 if __name__ == '__main__':
 	sys.exit(main())
