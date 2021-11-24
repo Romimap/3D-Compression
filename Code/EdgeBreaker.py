19d21245854778ec5b688de2a4244ba3ae093e91
FeuFeve <clement-potin@outlook.com>
2021-11-24 13:11:01 +0100

    Update EdgeBreaker.py with initial configuration finding

--- Code/EdgeBreaker.py
+++ Code/EdgeBreaker.py
@@ -14,12 +14,20 @@ import numpy
 # Global variables
 # ------------------------------------------------------------
 
-_heMesh = None 			# The mesh containing half-edges data
+_heMesh = None 				# The mesh containing half-edges data
 
-_vertices = []			# List of int references to vertices: V in the paper
-_oppositeCorners = []	# List of int references to opposite corners: O in the paper
-_marked = []			# List of bool indicating whether a vertex has already been visited: M in the paper
-_flagged = []			# List of bool indicating whether a triangle has already been visited: U in the paper
+_deltas = []				# List of 3D points/vectors storing the first points and the correction vectors
+_clers = ""					# String storing the CLERS steps of the EdgeBreaker algorithm's path
+
+# _vertices = []				# List of int references to vertices: V in the paper
+# _oppositeCorners = []		# List of int references to opposite corners: O in the paper
+_marked = []				# List of bool indicating whether a vertex has already been visited: M in the paper
+_flagged = []				# List of bool indicating whether a triangle has already been visited: U in the paper
+
+_missingTrianglesCount = 0	# The number of triangles not already seen by EdgeBreaker
+
+_halfEdgeId = 0				# Select first half-edge to begin the EdgeBreaker algorithm
+_triangleId = 0				# Select the first triangle (always associated with first halfEdge)
 
 
 # ------------------------------------------------------------
@@ -29,33 +37,77 @@ _flagged = []			# List of bool indicating whether a triangle has already been vi
 ## VERTICES
 
 def getVertexId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
 	return _heMesh.half_edges[halfEdgeId].vertex_indices[0]
 
 
 def getNextVertexId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
 	return getVertexId(getNextHeId(halfEdgeId))
 
 
 def getPreviousVertexId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
 	return getVertexId(getPreviousHeId(halfEdgeId))
+
+
+def getOppositeVertexId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
+	return getVertexId(getOppositeCornerHeId(halfEdgeId))
+
+
+def getLeftVertexId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
+	return getVertexId(getLeftCornerHeId(halfEdgeId))
+
+
+def getRightVertexId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
+	return getVertexId(getRightCornerHeId(halfEdgeId))
 
 
 ## HALF-EDGES
 
 def getNextHeId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
 	return _heMesh.half_edges[halfEdgeId].next
 
 
 def getPreviousHeId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
 	return getNextHeId(getNextHeId(halfEdgeId))
 
 
 def getTwinHeId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
 	return _heMesh.half_edges[halfEdgeId].twin
 
 
-def getOppositeCornerHEId(halfEdgeId):
+def getOppositeCornerHeId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
 	return getPreviousHeId(getTwinHeId(getNextHeId(halfEdgeId)))
+
+
+def getLeftCornerHeId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
+	return getPreviousHeId(getTwinHeId(halfEdgeId))
+
+
+def getRightCornerHeId(halfEdgeId):
+	if halfEdgeId == -1:
+		return -1
+	return getPreviousHeId(getTwinHeId(getPreviousHeId(halfEdgeId)))
 
 
 ## DISTANCE VECTORS
@@ -72,8 +124,48 @@ def getDistanceVectorFromHeId(fromHeId, toHeId):
 
 ## VERTEX POSITION
 
+def getVertexPosFromVertexId(vertexId):
+	return _heMesh.vertices[vertexId]
+
+
 def getVertexPosFromHeId(halfEdgeId):
 	return _heMesh.vertices[getVertexId(halfEdgeId)]
+
+
+## MARKS/FLAGS
+
+def mark(halfEdgeId):
+	global _marked
+
+	if halfEdgeId != -1:
+		_marked[getVertexId(halfEdgeId)] = True
+
+
+def isMarked(halfEdgeId):
+	global _marked
+
+	if halfEdgeId == -1:
+		return False
+	else:
+		return _marked[getVertexId(halfEdgeId)]
+
+
+def flag(halfEdgeId):
+	global _flagged
+
+	if halfEdgeId != -1:
+		triangleId = _heMesh.half_edges[halfEdgeId].triangle_index
+		_flagged[triangleId] = True
+
+
+def isFlagged(halfEdgeId):
+	global _flagged
+	
+	if halfEdgeId == -1:
+		return False
+	else:
+		triangleId = _heMesh.half_edges[halfEdgeId].triangle_index
+		return _flagged[triangleId]
 
 
 # ------------------------------------------------------------
@@ -92,17 +184,26 @@ def printMeshInfo():
 # ------------------------------------------------------------
 
 def initData():
-	global _vertices, _oppositeCorners, _marked, _flagged
+	global _marked, _flagged, _missingTrianglesCount
 
-	# Vertices and opposite corners
-	halfEdgeCount = len(_heMesh.half_edges)
-	for halfEdgeId in range(halfEdgeCount):
-		_vertices.append(getVertexId(halfEdgeId))
-		_oppositeCorners.append(getOppositeCornerHEId(halfEdgeId))
+	# # Vertices and opposite corners
+	# halfEdgeCount = len(_heMesh.half_edges)
+	# for halfEdgeId in range(halfEdgeCount):
+	# 	_vertices.append(getVertexId(halfEdgeId))
+	# 	_oppositeCorners.append(getOppositeCornerHeId(halfEdgeId))
 
 	# Marks and flags
-	_marked = [False] * len(_heMesh.vertices) 	# Marked: if a vertex has been visited or not
-	_flagged = [False] * halfEdgeCount			# Flags: if a triangle's corner has been visited or not
+	_marked = [False] * len(_heMesh.vertices) 		# Marks: if a vertex has been visited or not
+	_flagged = [False] * len(_heMesh.triangles)		# Flags: if a triangle has been visited or not
+	_missingTrianglesCount = len(_heMesh.triangles)	# No triangle has been seen yet
+
+	# Mark boundary vertices as "seen"
+	for halfEdge in _heMesh.half_edges:
+		if halfEdge.twin == -1:
+			_marked[halfEdge.vertex_indices[0]] = True
+			_marked[halfEdge.vertex_indices[1]] = True
+			
+	print("End of initData()")
 
 
 
@@ -115,27 +216,50 @@ def initData():
 #     U[c.t] = 1;                                 # paint the triangle and go to opposite corner
 #     Compress (c.o); }                           # start the compression process
 def initCompression():
-	initData() # GLOBAL M[]={0...}, U[]={0...} + V & O initialization from _heMesh
+	global _halfEdgeId, _triangleId, _clers, _deltas
 
-	deltas = [] # Deltas: first vertex and squence of corrective vertors from current to next vertex
+	initData()	# GLOBAL M[]={0...}, U[]={0...} + V & O initialization from _heMesh
 
-	halfEdgeId = 0 # Select first half-edge to begin the EdgeBreaker algorithm
+	# Try to find a 'C' or an 'S' configuration by rotating the first vertex in the current triangle
+	for i in range(6):
+		print(str(i) + " (He id) left: " + str(getLeftCornerHeId(_halfEdgeId)) + ", right: " + str(getRightCornerHeId(_halfEdgeId)))
+		print(str(i) + " (Vertex id) left: " + str(getLeftVertexId(_halfEdgeId)) + ", right: " + str(getRightVertexId(_halfEdgeId)))
+		if i < 3:
+			if getLeftCornerHeId(_halfEdgeId) != -1 and getRightCornerHeId(_halfEdgeId) != -1:
+				if isMarked(_halfEdgeId):
+					print(str(i) + " found an S configuration")
+					break
+				else:
+					print(str(i) + " found an C configuration")
+					break
+		else:
+			if getLeftCornerHeId(_halfEdgeId) != -1:
+				print(str(i) + " found an R configuration")
+				break
+			if getRightCornerHeId(_halfEdgeId) != -1:
+				print(str(i) + " found an L configuration")
+				break
+		print(str(i) + " turn")
+		_halfEdgeId = getNextHeId(_halfEdgeId)
+		if i == 5:
+			print(str(i) + " found an E configuration")
+
 	# First vertex position
-	deltas.append(getVertexPosFromHeId(getPreviousHeId(halfEdgeId))) 					# WRITE(delta, c.p.v.g)
+	_deltas.append(getVertexPosFromHeId(getPreviousHeId(_halfEdgeId))) 					# WRITE(delta, c.p.v.g)
 	# Vector from first to second vertex
-	deltas.append(getDistanceVectorFromHeId(getPreviousHeId(halfEdgeId), halfEdgeId)) 	# WRITE(delta, c.v.g – c.p.v.g)
+	_deltas.append(getDistanceVectorFromHeId(getPreviousHeId(_halfEdgeId), _halfEdgeId)) # WRITE(delta, c.v.g – c.p.v.g)
 	# Vector from second to third vertex
-	deltas.append(getDistanceVectorFromHeId(halfEdgeId, getNextHeId(halfEdgeId))) 		# WRITE(delta, c.n.v.g – c.v.g)
+	_deltas.append(getDistanceVectorFromHeId(_halfEdgeId, getNextHeId(_halfEdgeId))) 	# WRITE(delta, c.n.v.g – c.v.g)
 
-	# Mark these vertices
-	_marked[_vertices[halfEdgeId]] = True 					# M[c.v] = 1
-	_marked[_vertices[getNextHeId(halfEdgeId)]] = True 		# M[c.n.v] = 1
-	_marked[_vertices[getPreviousHeId(halfEdgeId)]] = True 	# M[c.p.v] = 1
+	# Mark these vertices as "seen"
+	mark(_halfEdgeId)					# M[c.v] = 1
+	mark(getNextHeId(_halfEdgeId))		# M[c.n.v] = 1
+	mark(getPreviousHeId(_halfEdgeId))	# M[c.p.v] = 1
 
-	# _flagged[] // STOPPED HERE
+	# Flag the triangle as "seen"
+	flag(_halfEdgeId)	# U[c.t] = 1
 
-	print(numpy.asarray(_heMesh.triangles))
-	print(numpy.asarray(_heMesh.half_edges))
+	print("End of initCompression()")
 
 
 # ------------------------------------------------------------
@@ -144,10 +268,17 @@ def initCompression():
 
 def main():
 	global _heMesh
+
 	print("Running MAIN from EdgeBreaker.py")
-	mesh = open3d.io.read_triangle_mesh("Models/cube.obj")
+	mesh = open3d.io.read_triangle_mesh("Models/triangle.obj")
 	_heMesh = open3d.geometry.HalfEdgeTriangleMesh.create_from_triangle_mesh(mesh)
+	
 	initCompression()
+
+	# open3d.visualization.draw_geometries([_heMesh])
+	# print(numpy.asarray(_heMesh.triangles))
+	# print(numpy.asarray(_heMesh.half_edges))
+
 	return 0
 
 
